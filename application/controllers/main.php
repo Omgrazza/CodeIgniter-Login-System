150239585e31265b0d65ae985813692278b6fbf7
omgrazza <admin@sv-rs.org>
2015-01-28 23:05:10 +0000

    Added the index and login functions

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -4,6 +4,32 @@ class Main extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		// Check if the is logged in and has a active session.
+		if ($this->session->user_data('logged_in') == true)
+		{
+			// Load the index view.
+		}
+		else
+		{
+			// Redirect the user to the login page.
+			redirect('login', 'refresh');
+			// Kill the script to prevent header modification.
+			exit();
+		}
+	}
+
+	public function login()
+	{
+		// Check if the user is already logged in.
+		if ($this->session->userdata('logged_in') == true) {
+			// Redirect the user to the index page.
+			redirect('index', 'refresh');
+			// Kill the script to prevent header modification.
+			exit();
+		}
+		else
+		{
+
+		}
 	}
 }
