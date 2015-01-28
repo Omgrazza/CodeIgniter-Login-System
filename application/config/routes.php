984ab1b96ae09865ec21ea193a9f4f5d532e636f
omgrazza <admin@sv-rs.org>
2015-01-28 04:42:36 +0000

    Changed the default controller and added a custom route for our htaccess (cleaner URL's).

--- application/config/routes.php
+++ application/config/routes.php
@@ -38,9 +38,9 @@
 |
 */
 
-$route['default_controller'] = "welcome";
+$route['default_controller'] = "main";
 $route['404_override'] = '';
-
+$route['(:any)'] = 'main/$1';
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
